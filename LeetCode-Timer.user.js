659d074361d79651a11e5383d1cd35512651f251
yuki <abbq100@163.com>
2020-06-30 23:30:08 -0700

    upgrade to version 0.8

--- LeetCode-Timer.user.js
+++ LeetCode-Timer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leetcode Timer
 // @namespace    https://github.com/YukiDayDreamer/LeetCode-Timer/
-// @version      0.7
+// @version      0.8
 // @description  Mount a Timer for Leetcode Problem
 // @author       YukiDayDreamer
 // @updateURL    https://raw.githubusercontent.com/YukiDayDreamer/LeetCode-Timer/master/LeetCode-Timer.meta.js
